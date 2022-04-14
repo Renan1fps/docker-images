d8bc2d031d5d13d1f373a4623aa2f0cd7b59ca86
Renan <renan.sergipe68@gmail.com>
2022-04-14 12:11:06 -0300

    feat: server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+const { default: axios } = require("axios")
 const { urlencoded } = require("express")
 const express = require("express")
 
 const app = express()
 
 app.use(express.json())
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({ extended: true }))
 
-app.get("/image", (req, res)=>{
-  res.json({message: "Image Docker running"})
+app.get("/test", async (req, res) => {
+  res.send('ok2')
 })
 
-app.listen(8080, ()=>{
+app.listen(4014, () => {
   console.log("Server running")
 })
