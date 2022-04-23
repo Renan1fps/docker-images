a68dfd88244a63462153dc06f3df0c9c28ea5f5f
Renan Oliveira <renan.sergipe68@gmail.com>
2022-04-22 22:31:17 -0300

    fix: bugfix cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const { default: axios } = require("axios")
 const { urlencoded } = require("express")
 const express = require("express")
+const cors = require ('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
+app.use(cors())
 
 app.get("/test", async (req, res) => {
   res.send(`API online ${new Date()}`)
